d0a26fa7ecc3a30805267e389cc961fe05c8bbc0
minimomo14 <93943633+minimomo14@users.noreply.github.com>
2023-11-13 23:20:30 -0500

    added switch theme function

--- assets/js/app.js
+++ assets/js/app.js
@@ -42,3 +42,69 @@ function scrollActive() {
 }
 
 window.addEventListener('scroll', scrollActive)
+
+// change background header
+
+function scrollHeader() {
+    const nav = document.getElementById('header')
+
+    //when the scroll is greater than 200viewport height, add the scroll header class to the header tag
+    if(this.scrollY >= 200) nav.classList.add('scroll-header'); else nav.classList.remove('scroll-header')
+
+}
+window.addEventListener('scroll', scrollHeader);
+
+//scroll top function
+function scrollTop(){
+    const scrollTop = document.getElementById('scrolltop');
+    //when the scroll is higher than 560 viewport height, add the show scroll class to the tag
+    if(this.scrollY >= 560) scrollTop.classList.add('show-scroll');
+    else scrollTop.classList.remove('show-scroll')
+}
+window.addEventListener('scroll', scrollTop)
+
+// switch theme mode
+const themeButton = document.getElementById('theme-button');
+const darkTheme = 'dark-theme';
+const iconTheme = 'fa-moon'
+
+//if user selected
+const selectedTheme = localStorage.getItem('selected-theme');
+const selectedIcon = localStorage.getItem('selected-icon')
+
+//obtain the current theme that the interface has by validating the dark-theme class
+const getCurrentTheme = () => document.body.classList.contains(darkTheme) ? 'dark' : 'light'
+const getCurrentIcon = () => themeButton.classList.contains(iconTheme) ? 'fa-moon' : 'fa-sun'
+
+if (selectedTheme) {
+    document.body.classList.[selectedTheme === 'dark' ? 'add' : 'remove']
+    (darkTheme)
+    themeButton.classList[selectedIcon === 'fa-moon' ? 'add' : 'remove']
+    (iconTheme)
+}
+
+//activate/ deactivate the theme manually with button
+themeButton.addEventListener('click', () => {
+    //add or remove the dark/icon theme
+    document.body.classList.toggle(darkTheme);
+    themeButton.classList.toggle(iconTheme);
+
+    //save the theme and the current icon that user choose
+    localStorage.setItem('selected-theme', getCurrentTheme());
+    localStorage.setItem('selected-icon', getCurrentIcon());
+})
+
+// scroll animation!!!
+
+const sr = scrollReveal({
+    origin: 'top',
+    distance: '30px',
+    duration: 2000,
+    reset: true
+});
+
+sr.reveal(`.home_data, .home_img, .about_data, .about_img,
+            .services_content, .menu_content,
+            .contact_data, .contact_button, .footer_content` , {
+                interval: 200
+            })
